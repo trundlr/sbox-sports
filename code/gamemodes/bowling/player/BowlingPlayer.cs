9267619550b66a6964d1c84097f71e4ffcd63398
Alex <alex@gu3.me>
2022-05-16 18:42:44 +0100

    Basic bowling ball logic, move BowlingPlayer into player folder

---
+++ code/gamemodes/bowling/player/BowlingPlayer.cs
@@ -0,0 +1,40 @@
+﻿namespace Sports;
+
+public class BowlingPlayer : Player
+{
+	public override void Respawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		Animator = new StandardPlayerAnimator();
+		CameraMode = new FirstPersonCamera();
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		base.Respawn();
+	}
+
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		Entity.All.Where( x => x.Owner == this ).OfType<BowlingBall>().ToList().ForEach( x => x.Simulate() );
+
+		if ( !IsServer )
+			return;
+
+		if ( Input.Pressed( InputButton.Attack1 ) )
+		{
+			var bowlingBall = new BowlingBall();
+			var tr = Trace.Ray( EyePosition, EyePosition + EyeRotation.Forward * 64 ).Ignore( this ).Run();
+
+			bowlingBall.Position = tr.EndPosition + tr.Normal * 32;
+			bowlingBall.Velocity = EyeRotation.Forward * 512;
+			bowlingBall.Owner = this;
+		}
+	}
+}
