3827e68c8215acdb053644b26efd12299c1468d1
Alex <alex@gu3.me>
2022-05-16 18:04:45 +0100

    Add basic bowling player pawn

---
+++ code/gamemodes/bowling/BowlingPlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace Sports;
+
+public class BowlingPlayer : Player
+{
+	public override void Respawn()
+	{
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		Controller = new WalkController();
+		Animator = new StandardPlayerAnimator();
+		CameraMode = new FirstPersonCamera();
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		base.Respawn();
+	}
+}
