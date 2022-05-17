d66f9cbc3976b008ff8d073e8a778919e087883f
Tony Ferguson <devultj@gmail.com>
2022-05-17 01:18:29 +0100

    Stub for Test Gamemode

---
+++ code/Base/GamemodeSystem/TestGamemode.cs
@@ -0,0 +1,9 @@
+namespace Sports;
+
+[Library( "sports_test_gamemode" )]
+[Hammer.EditorModel( "models/Bowling/bpin.vmdl" )]
+[Display( Name = "Test Gamemode" )]
+public partial class TestGamemode : BaseGamemode
+{
+
+}
