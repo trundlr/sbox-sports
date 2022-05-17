ac5d4767a999af1a1a625395aaf54a345b26ecee
Tony Ferguson <devultj@gmail.com>
2022-05-17 01:42:30 +0100

    Add stub gamemode orb entity

---
+++ code/Base/GamemodeSystem/GamemodeOrb.cs
@@ -0,0 +1,11 @@
+namespace Sports;
+
+[Library( "sports_gamemode_orb" )]
+[EditorModel( "models/dev/gamemode_orb.vmdl" )]
+[Display( Name = "Gamemode Orb" )]
+[Category( "Map Setup" )]
+[Sphere( 128f, 0, 125, 255 )]
+public partial class GamemodeOrb : AnimEntity
+{
+
+}
--- code/Base/GamemodeSystem/TestGamemode.cs
+++ code/Base/GamemodeSystem/TestGamemode.cs
@@ -1,8 +1,10 @@
 namespace Sports;
 
 [Library( "sports_test_gamemode" )]
-[Hammer.EditorModel( "models/Bowling/bpin.vmdl" )]
+[EditorModel( "models/Bowling/bpin.vmdl" )]
 [Display( Name = "Test Gamemode" )]
+[Category( "Gamemodes" )]
+[Sphere( 128f, 0, 125, 255 )]
 public partial class TestGamemode : BaseGamemode
 {
 
