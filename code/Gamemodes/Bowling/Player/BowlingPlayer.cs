bb822c4329ad39b50168f066367bdf016d0a82d5
Alex <alex@gu3.me>
2022-05-18 21:59:34 +0100

    Bowling should use new gamemode & player systems

--- code/Gamemodes/Bowling/Player/BowlingPlayer.cs
+++ code/Gamemodes/Bowling/Player/BowlingPlayer.cs
@@ -1,21 +1,12 @@
 ﻿namespace Sports;
 
-public class BowlingPlayer : Player
+public class BowlingPlayer : BasePlayer
 {
 	public override void Respawn()
 	{
-		SetModel( "models/citizen/citizen.vmdl" );
-
-		Controller = new WalkController();
-		Animator = new StandardPlayerAnimator();
-		CameraMode = new FirstPersonCamera();
-
-		EnableAllCollisions = true;
-		EnableDrawing = true;
-		EnableHideInFirstPerson = true;
-		EnableShadowInFirstPerson = true;
-
 		base.Respawn();
+
+		Camera = new ThirdPersonCamera();
 	}
 
 	public override void Simulate( Client cl )
