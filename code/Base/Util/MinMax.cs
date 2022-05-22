79cacc90b4a80efa8d746f16df7a9989875afaba
Tony Ferguson <devultj@gmail.com>
2022-05-22 17:17:18 +0100

    Added MinMax<T>

---
+++ code/Base/Util/MinMax.cs
@@ -0,0 +1,12 @@
+namespace Sports;
+public struct MinMax<T> where T : unmanaged
+{
+    public T Min;
+    public T Max;
+
+    public MinMax( T min, T max )
+    {
+        Min = min;
+        Max = max;
+    }
+}
