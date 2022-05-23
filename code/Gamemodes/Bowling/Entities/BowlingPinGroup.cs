b673c1a59585fbee9deef414de7d2f0587647a49
Doctor Gurke <docgurk@gmail.com>
2022-05-23 02:38:59 +0200

    temp 10-pin pin group entity

---
+++ code/Gamemodes/Bowling/Entities/BowlingPinGroup.cs
@@ -0,0 +1,22 @@
+﻿namespace Sports;
+
+[HammerEntity]
+[EditorModel( "models/bowling/pin_group.vmdl" )]
+public class BowlingPinGroup : Entity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+	}
+
+	[Event.Tick.Server]
+	public void ServerTick()
+	{
+		if ( !Debug.Enabled ) return;
+
+		DebugOverlay.Text( "pingroup", Position );
+		DebugOverlay.Sphere( Position, 5, Color.White );
+	}
+}
