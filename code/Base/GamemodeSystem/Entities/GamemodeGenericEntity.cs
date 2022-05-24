f58e6b48b60007630bffcd1a849b7a342047a38c
Doctor Gurke <docgurk@gmail.com>
2022-05-25 00:29:31 +0200

    comment

--- code/Base/GamemodeSystem/Entities/GamemodeGenericEntity.cs
+++ code/Base/GamemodeSystem/Entities/GamemodeGenericEntity.cs
@@ -13,6 +13,8 @@ public class GamemodeGenericEntity : GamemodeEntity
 	{
 		base.Spawn();
 
+		// Ideally we'd transmit this to participants of the gamemode but we don't really
+		// have control over that just yet.
 		Transmit = TransmitType.Always;
 	}
 
