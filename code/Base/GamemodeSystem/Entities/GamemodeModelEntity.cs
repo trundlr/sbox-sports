3bdeb85bf175a53dde763b482e14a15f9a4708e2
Doctor Gurke <docgurk@gmail.com>
2022-05-24 16:18:43 +0200

    add GamemodeModelEntity and GamemodeAnimatedEntity to be used for gamemode entities going forwards

---
+++ code/Base/GamemodeSystem/Entities/GamemodeModelEntity.cs
@@ -0,0 +1,28 @@
+namespace Sports;
+
+[HammerEntity]
+[Title( "Generic Gamemode ModelEntity" )]
+[Category( "Gamemode Setup" )]
+[Icon( "transform" )]
+[EditorModel( "models/editor/info_target.vmdl" )]
+public partial class GamemodeModelEntity : ModelEntity, IGamemodeEntity
+{
+	[Property( Title = "Gamemode Name" ), Net]
+	public string GamemodeName { get; set; }
+	[Net]
+	public BaseGamemode Gamemode { get; set; }
+
+	public override string ToString()
+	{
+		return $"{ClassName}:[{Gamemode.ClassName}] ({string.Join( ", ", Tags.List )})";
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		// Ideally we'd transmit this to participants of the gamemode but we don't really
+		// have control over that just yet.
+		Transmit = TransmitType.Always;
+	}
+}
