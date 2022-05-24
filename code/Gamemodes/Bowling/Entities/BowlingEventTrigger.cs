77e579d7f7ec91b9ca0e08379a02e3ecd3907661
Doctor Gurke <docgurk@gmail.com>
2022-05-24 23:57:33 +0200

    added initial event trigger

---
+++ code/Gamemodes/Bowling/Entities/BowlingEventTrigger.cs
@@ -0,0 +1,37 @@
+﻿namespace Sports;
+
+[Solid]
+[HammerEntity]
+[Category( "Bowling" )]
+[Title( "Event Trigger" )]
+public class BowlingEventTrigger : GamemodeBaseTrigger
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+
+		ActivationTags.Add( "bowling_ball" );
+	}
+
+	public override void OnTouchStart( Entity toucher )
+	{
+		base.OnTouchStart( toucher );
+
+		if ( toucher is BowlingBall ball )
+		{
+			Log.Debug( $"event triggr ball start touch {ball}" );
+		}
+	}
+
+	public override void EndTouch( Entity other )
+	{
+		base.EndTouch( other );
+
+		if ( other is BowlingBall ball )
+		{
+			Log.Debug( $"event trigger ball end touch {ball}" );
+		}
+	}
+}
