f6cc033c1e1dbb7bf80b708ecdf2af4b99800803
Doctor Gurke <docgurk@gmail.com>
2022-05-24 17:21:29 +0200

    Update BowlingPitTrigger.cs

--- code/Gamemodes/Bowling/Entities/BowlingPitTrigger.cs
+++ code/Gamemodes/Bowling/Entities/BowlingPitTrigger.cs
@@ -1,31 +1,11 @@
 ﻿namespace Sports;
 
-public partial class BowlingPitTrigger : BaseTrigger
+public partial class BowlingPitTrigger : GamemodeBaseTrigger
 {
-	[Net, Predicted]
-	public BaseGamemode Gamemode { get; set; }
-
-	[Property( "gamemode_name", Title = "Gamemode Name" ), FGDType( "target_destination" )]
-	public string GamemodeName { get; set; }
-
 	public override void Spawn()
 	{
 		base.Spawn();
 
 		Transmit = TransmitType.Always;
-
-		LinkGamemode();
-	}
-
-	private void LinkGamemode()
-	{
-		var gamemode = All.OfType<BaseGamemode>().First( x => x.Name == GamemodeName );
-		if ( !gamemode.IsValid() )
-		{
-			Log.Warning( $"Entity {ClassName} failed to link with valid gamemode!" );
-			return;
-		}
-
-		Gamemode = gamemode;
 	}
 }
